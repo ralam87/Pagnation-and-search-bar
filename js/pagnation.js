a0f8c84277a97ca918d63e650ab672341da173ca
ralam87 <Ruhull.alam@outlook.com>
2017-01-18 18:32:56 +0000

    search bar amendment
    changed search bar function to work properly straight away , originally
    would only work if a  page  was clicked first.

--- js/pagnation.js
+++ js/pagnation.js
@@ -10,6 +10,7 @@ Created by Ruhull Alam Dec 2016
 
 ////////////////////////////////////pagnation code/////////////////////////////////// 
 
+
 var pageNum = 0; // a changing variable that decides what number should be placed in the var textNode
 var liItem = document.getElementsByClassName("student-item cf"); //consolidates all students by the class tag
 var limitPerPage = 10; //set how many items to show per page
@@ -28,41 +29,37 @@ var pagesRequired = Math.floor(liItem.length/limitPerPage); //figures out how ma
 
 
 //This function adds the pagination li after taking variable from pageNum and applying it to textNode
-var createPage = function (pageNum) 
-{
-
-
+var createPage = function (pageNum) { 
     
-    
-var divPag = document.getElementById("idPag");
-var liPag = document.createElement("li");
-divPag.append(liPag); //Hey create a li element and attach it to the ul element.
-liPag.append(pageNum); //for each li page item added, put the page number on it...please.
+	var divPag = document.getElementById("idPag");
+	var liPag = document.createElement("li");
+	divPag.append(liPag); //Hey create a li element and attach it to the ul element.
+	liPag.append(pageNum); //for each li page item added, put the page number on it...please.
 
-    
-liPag.setAttribute("id", "page" + pageNum); //this adds an id to the relevent pageNum
-liPag.setAttribute("class", "pagination")//this adds the pagination class;
-    document.getElementById("page"+pageNum).addEventListener("click", function() 
-        {
-        var x = "page"+pageNum;
-        var pageCount = document.getElementsByTagName("ul")[1].childElementCount;
+		
+	liPag.setAttribute("id", "page" + pageNum); //this adds an id to the relevent pageNum
+	liPag.setAttribute("class", "pagination")//this adds the pagination class;
+		document.getElementById("page"+pageNum).addEventListener("click", function() 
+			{
+			var x = "page"+pageNum;
+			var pageCount = document.getElementsByTagName("ul")[1].childElementCount;
 
 
-//Below sets each page number instructions to say which results are meant to be shown which page        
-    if (x) {
-           hideAll();
-           //this allows the range (results to be shown) to be set dependent on how many pages there are. 
-             for (j = 0;  j < pageCount; j++) 
-             {
-             if (x === "page"+[j]) 
-               {
-               showResults(([j]-1) * limitPerPage, ([j] * limitPerPage)); 
-             //e.g page 7 would be 7-1 (6) * 10 (total 60), 7 * 10 (total 70), so the range would be set between 60 and 70 etc..
-                } 
-              }
-           }
-                
-         });    
+	//Below sets each page number instructions to say which results are meant to be shown which page        
+		if (x) {
+			   hideAll();
+			   //this allows the range (results to be shown) to be set dependent on how many pages there are. 
+				 for (j = 0;  j < pageCount; j++) 
+				 {
+				 if (x === "page"+[j]) 
+				   {
+				   showResults(([j]-1) * limitPerPage, ([j] * limitPerPage)); 
+				 //e.g page 7 would be 7-1 (6) * 10 (total 60), 7 * 10 (total 70), so the range would be set between 60 and 70 etc..
+					} 
+				  }
+			   }
+					
+			 });    
 }
 
 
@@ -75,15 +72,17 @@ function hideAll()
   liItem[i].style.display = "none";   
   } 
 }
+
+   var showing = document.createElement("p");
+          showing.setAttribute("id", "showing");
+          document.getElementsByTagName("h2")[0].appendChild(showing);
+          
     
     //this function displays the results after receiving a range linked to each pageNum li
       function showResults(startRange, endRange) {
                  for (j = startRange; j < endRange && j < liItem.length; j++) 
                liItem[j].style.display = "block";
-          var showing = document.createElement("p");
-          showing.setAttribute("id", "showing");
-          document.getElementsByTagName("h2")[0].appendChild(showing);
-          
+       
           document.getElementById("showing").innerHTML = "Showing results: ";
           document.getElementById("showing").append(startRange + "-" + (checkResultsNum(startRange, limitPerPage)) + " of " + liItem.length);
       } 
@@ -157,7 +156,7 @@ var counter=0;
 var searchButton = document.getElementById("searchForm").addEventListener("click", function() {
     // the request (made in #searchForm) is compared to the value in the h3 content of each item in the student-item cf
 
-var request = document.getElementById("inputBox").value;
+var request = document.getElementById("inputBox").value; 
     if (request) {
         for (y = 0; y < liItem.length; y++) {
         if (h3Names[y].innerHTML.indexOf(request.toLowerCase()) > -1) {
@@ -167,19 +166,20 @@ var request = document.getElementById("inputBox").value;
             } else {
             liItem[y].style.display = "none";
             counter++;
-                if (counter === liItem.length) {
+                if (counter === liItem.length + 1) {
                 document.getElementById("showing").textContent = "No search results";
                     counter=0;
                     
                   }
                 
              }
-    } 
-} else {
-  showResults(0, 10)  
-};
-}
+				} 
+			} else {
+				showResults(0, 10)  
+		};
+	}
 );
+
 
 
 ////////////////////////////////end of search tool code//////////////////////////////////////////////
